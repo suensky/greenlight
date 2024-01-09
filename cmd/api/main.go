6054752245536673d87ddc2e6f594f6e98fef3f6
suensky <suensky@outlook.com>
2024-01-09 14:40:52 -0800

    ch10 JSON logger and Panic recovery

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,6 +12,7 @@ import (
 
 	_ "github.com/lib/pq"
 	"github.com/suensky/greenlight/internal/data"
+	"github.com/suensky/greenlight/internal/jsonlog"
 )
 
 const version = "1.0.0"
@@ -30,7 +31,7 @@ type config struct {
 
 type application struct {
 	config config
-	logger *log.Logger
+	logger *jsonlog.Logger
 	models data.Models
 }
 
@@ -47,16 +48,16 @@ func main() {
 	flag.Parse()
 
 	// Logger
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
 	// DB
 	cfg.db.dsn = fmt.Sprintf("postgres://greenlight:%s@localhost/greenlight?sslmode=disable", cfg.db.dbPassword)
 	db, err := openDB(cfg)
 	if err != nil {
-		logger.Fatal(err)
+		logger.PrintFatal(err, nil)
 	}
 	defer db.Close()
-	logger.Println("database connection pool established")
+	logger.PrintInfo("database connection pool established", nil)
 
 	app := &application{
 		config: cfg,
@@ -67,14 +68,15 @@ func main() {
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
+		ErrorLog:     log.New(logger, "", 0),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
-	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	logger.PrintInfo(fmt.Sprintf("starting %s server on %s", cfg.env, srv.Addr), nil)
 	err = srv.ListenAndServe()
-	logger.Fatal(err)
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
